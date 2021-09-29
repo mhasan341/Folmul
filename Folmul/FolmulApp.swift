7c2c069412b535d1db41204b5a4a53040e2c068b
Mahmudul Hasan <mhasan341@gmail.com>
2021-09-30 02:20:14 +0600

    Initial Commit

---
+++ Folmul/FolmulApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolmulApp.swift
+//  Folmul
+//
+//  Created by Mahmud on 2021-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FolmulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
